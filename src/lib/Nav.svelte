694d79dc971d57f0c7f322b1c8188a7e402ae784
Flora Fischbacher <florafischbacher1@gmail.com>
2021-10-01 14:15:33 +0200

    Fixed navbar issue

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -29,7 +29,7 @@
         <button
           class="flex items-center px-3 py-2 rounded-sm bg-brand-dust-rose text-brand-off-white hover:bg-brand-brown"
           aria-label="Hamburger menu"
-          on:click="{toggleHamburgerMenu}"
+          on:click="{() => toggleHamburgerMenu()}"
         >
           <Icon data="{faBars}" class="text-xl" />
         </button>
