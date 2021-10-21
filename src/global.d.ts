f72f53f1efba2a178e8a4064ebd640cbc2499d27
mhatvan <markus_hatvan@aon.at>
2021-10-21 17:03:57 +0200

    fix: mobile menu is open by default
    - remove svelte-click-outside and use own hook
    - fix navbar going into second column on smaller resolutions

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,5 +3,10 @@
 /// <reference types="vite/client" />
 
 // added stub declarations for these deps to silence svelte-check warnings
-declare module 'svelte-click-outside';
 declare module 'svelte-awesome';
+
+declare namespace svelte.JSX {
+  interface HTMLProps<T> {
+    onclickOutside?: () => void;
+  }
+}
